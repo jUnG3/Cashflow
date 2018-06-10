9dbdab5a6640163919bef50dd16302fe8d0e9343
jUnG3 <soldo.stjepan1304@gmail.com>
2018-06-10 20:26:25 +0200

    Fixes messages

--- app/src/main/java/de/cashflow/cashflow/login/view/UsernameAndPasswordView.kt
+++ app/src/main/java/de/cashflow/cashflow/login/view/UsernameAndPasswordView.kt
@@ -3,6 +3,7 @@ package de.cashflow.cashflow.login.view
 import android.content.Intent
 import android.widget.EditText
 import android.widget.Toast
+import de.cashflow.cashflow.R
 import de.cashflow.cashflow.dashboard.activity.DashboardActivity
 import de.cashflow.cashflow.login.activity.LoginActivity
 import de.cashflow.domain.model.User
@@ -10,11 +11,11 @@ import de.cashflow.domain.model.User
 class UsernameAndPasswordView(private val usernameView: EditText, private val passwordView: EditText) : LoginView {
 
     override fun showErrorMessage() {
-        Toast.makeText(usernameView.context, "Hallo Error", Toast.LENGTH_SHORT).show()
+        Toast.makeText(usernameView.context, R.string.label_wrong_credentials, Toast.LENGTH_SHORT).show()
     }
 
     override fun showSuccessMessage() {
-        Toast.makeText(usernameView.context, "Hallo Success", Toast.LENGTH_SHORT).show()
+        Toast.makeText(usernameView.context, R.string.label_account_created, Toast.LENGTH_SHORT).show()
     }
 
     override fun getUserName() = usernameView.text.toString()
